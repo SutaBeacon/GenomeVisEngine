b2fe97699973334024ea93d5ef292beb948682bd
Suta <makehust@gmail.com>
2018-06-01 17:14:38 +0800

    flush the repo

--- Plot/index.js
+++
@@ -1,6 +0,0 @@
-import chart from './Renderer/ChartRenderer';
-import circular from './Renderer/CircularRenderer';
-const Plot = window.Plot || {};
-Plot.chart = chart;
-Plot.circular = circular;
-export default Plot;
